736fc0253fd0de01bdbb779a93938d47b7ce116f
bin <tuduweb@qq.com>
2020-12-15 22:14:57 +0800

    chooseLocation改用Promises语法糖(then)。

--- Weather/pages/weather/weather.js
+++ Weather/pages/weather/weather.js
@@ -15,38 +15,72 @@ Page({
     var that = this
     // console.log("hello")
     console.log(e)
+
     wx.chooseLocation({
       latitude: latitude,
       longitude : longitude,
-      success: function(res){
-        console.log(res.address)
-        latitude = res.latitude
-        longitude = res.longitude
-        wx.request({
-          url: 'https://restapi.amap.com/v3/geocode/regeo?key='+key+'&location='+longitude+','+latitude,
-          success: function(res2){
-            console.log(res2)
-            var adcode = res2.data.regeocode.addressComponent.adcode
-            that.setData({
-              local : res2.data.regeocode.formatted_address
-            })
-            wx.request({
-              url: 'https://restapi.amap.com/v3/weather/weatherInfo?key='+key+'&extensions=all&city='+adcode,
-              success: function (res3) {
-                console.log(res3);
-                that.setData({
-                  amapWeatherData : res3.data.forecasts[0].casts,
-                  updateTime : res3.data.forecasts[0].reporttime,
-                  //local : res3.data.forecasts[0].city,
-                  temperature : res3.data.forecasts[0].casts[0].daytemp,
-                  today : res3.data.forecasts[0].casts[0]
-                });
-              }
-            })
-          }
-        })
-      }
+    }).then((res)=>{
+      console.log(res.address)
+      latitude = res.latitude
+      longitude = res.longitude
+      //部分接口如 downloadFile, request, uploadFile, connectSocket, createCamera（小游戏）本身就有返回值， 它们的 promisify 需要开发者自行封装。
+      wx.request({
+        url: 'https://restapi.amap.com/v3/geocode/regeo?key='+key+'&location='+longitude+','+latitude,
+        success: function(res2){
+          console.log(res2)
+          var adcode = res2.data.regeocode.addressComponent.adcode
+          that.setData({
+            local : res2.data.regeocode.formatted_address
+          })
+          wx.request({
+            url: 'https://restapi.amap.com/v3/weather/weatherInfo?key='+key+'&extensions=all&city='+adcode,
+            success: function (res3) {
+              console.log(res3);
+              that.setData({
+                amapWeatherData : res3.data.forecasts[0].casts,
+                updateTime : res3.data.forecasts[0].reporttime,
+                //local : res3.data.forecasts[0].city,
+                temperature : res3.data.forecasts[0].casts[0].daytemp,
+                today : res3.data.forecasts[0].casts[0]
+              });
+            }
+          })
+        }
+      })
     })
+
+    // wx.chooseLocation({
+    //   latitude: latitude,
+    //   longitude : longitude,
+    //   success: function(res){
+    //     console.log(res.address)
+    //     latitude = res.latitude
+    //     longitude = res.longitude
+    //     wx.request({
+    //       url: 'https://restapi.amap.com/v3/geocode/regeo?key='+key+'&location='+longitude+','+latitude,
+    //       success: function(res2){
+    //         console.log(res2)
+    //         var adcode = res2.data.regeocode.addressComponent.adcode
+    //         that.setData({
+    //           local : res2.data.regeocode.formatted_address
+    //         })
+    //         wx.request({
+    //           url: 'https://restapi.amap.com/v3/weather/weatherInfo?key='+key+'&extensions=all&city='+adcode,
+    //           success: function (res3) {
+    //             console.log(res3);
+    //             that.setData({
+    //               amapWeatherData : res3.data.forecasts[0].casts,
+    //               updateTime : res3.data.forecasts[0].reporttime,
+    //               //local : res3.data.forecasts[0].city,
+    //               temperature : res3.data.forecasts[0].casts[0].daytemp,
+    //               today : res3.data.forecasts[0].casts[0]
+    //             });
+    //           }
+    //         })
+    //       }
+    //     })
+    //   }
+    // })
   },
   /**
    * Lifecycle function--Called when page load
